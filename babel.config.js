b1c0319f7d8d5da94729a1458af7423f0288ddcd
Bon Axl Feeser <bonaxl015@gmail.com>
2023-04-23 19:59:27 +0800

    Config files

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,29 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.json',
+            '.scss'
+          ],
+          root: ['./src'],
+          alias: {
+            '@': './src'
+          }
+        }
+      ]
+    ]
+  }
+}
