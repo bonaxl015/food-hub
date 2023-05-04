034a4918c6c5b9f13cafd39ea70ce82e10331881
Bon Axl Feeser <bonaxl015@gmail.com>
2023-05-05 00:14:30 +0800

    feat: create root component

---
+++ src/screens/index.tsx
@@ -0,0 +1,44 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import * as SplashScreen from 'expo-splash-screen'
+import {
+  MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider
+} from 'react-native-paper'
+import routes from '@/routes'
+
+const Stack = createNativeStackNavigator()
+SplashScreen.preventAutoHideAsync()
+
+setTimeout(() => {
+  SplashScreen.hideAsync()
+}, 3000)
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    primary: '#fe724c'
+  }
+}
+
+const App = () => {
+  return (
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          headerShown: false
+        }}>
+          {routes.map(item => (
+            <Stack.Screen
+              key={item.key}
+              name={item.name}
+              component={item.component}
+            />
+          ))}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  )
+}
+
+export default App
