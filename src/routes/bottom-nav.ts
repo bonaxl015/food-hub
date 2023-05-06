890f9d3c73e0bcab5937d041dd73f38457a89860
Bon Axl Feeser <bonaxl015@gmail.com>
2023-05-06 22:53:36 +0800

    feat: Add bottom navigation routes

---
+++ src/routes/bottom-nav.ts
@@ -0,0 +1,22 @@
+const bottomNavRoutes = [
+  {
+    key: 'home',
+    title: 'Home',
+    focusedIcon: 'home',
+    unfocusedIcon: 'home-outline'
+  },
+  {
+    key: 'cart',
+    title: 'cart',
+    focusedIcon: 'cart',
+    unfocusedIcon: 'cart-outline'
+  },
+  {
+    key: 'favorites',
+    title: 'favorites',
+    focusedIcon: 'heart',
+    unfocusedIcon: 'heart-outline'
+  }
+]
+
+export default bottomNavRoutes
