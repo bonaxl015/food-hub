cd14315b369b34a8108d8bf9b6d6f839e65ce30f
Bon Axl Feeser <bonaxl015@gmail.com>
2023-05-11 23:10:13 +0800

    feat: add header on home menu

---
+++ src/components/header/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import User from '@/assets/user.png'
+
+const Header = () => {
+  const openSideMenu = () => console.log('menu')
+
+  return (
+    <View className="flex flex-row items-center justify-between h-[80px] pt-[10px]">
+      <TouchableOpacity
+        onPress={openSideMenu}
+        activeOpacity={0.7}
+      >
+        <MaterialCommunityIcons
+          name="menu"
+          size={30}
+          color="#000"
+        />
+      </TouchableOpacity>
+      <View className="flex items-center gap-[4px]">
+        <Text className="text-[14px] font-bold text-[#8c9099]">
+          Deliver to
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={16}
+          />
+        </Text>
+        <Text className="text-[14px] font-bold text-[#fe724c]">
+          4102 Pretty View Lane
+        </Text>
+      </View>
+      <View>
+        <Image
+          className="h-[40px] w-[40px] rounded-[6px]"
+          source={User}
+        />
+      </View>
+    </View>
+  )
+}
+
+export default Header
