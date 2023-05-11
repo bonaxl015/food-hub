3849b50b892212dca9634c17f80f8fed674765db
Bon Axl Feeser <bonaxl015@gmail.com>
2023-05-11 23:23:01 +0800

    feat: add home screen page

---
+++ src/screens/app-container/home/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { ScrollView, Text } from 'react-native'
+import { TextInput } from 'react-native-paper'
+import Header from '@/components/header'
+import Categories from './components/categories'
+import Featured from './components/featured'
+import Popular from './components/popular'
+import { categories } from './data/categories'
+import { featured } from './data/featured'
+
+const Home = () => {
+  const [featuredData, setFeaturedData] = useState(featured)
+
+  const filterCategories = (data: any) => {
+    const filterData = featured.filter(
+      item => item.categories.includes(data.category)
+    )
+    setFeaturedData(filterData)
+  }
+
+  return (
+    <ScrollView
+      className="mx-[16px]"
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+    >
+      <Header />
+      <Text className="mt-[8px] mb-[16px] w-[300px] text-[28px] font-bold">
+        What would you like to order
+      </Text>
+      <TextInput
+        mode="outlined"
+        outlineColor="#9aa0b4"
+        placeholder="Find for food or restaurant"
+        left={
+          <TextInput.Icon icon="magnify" />
+        }
+      />
+      <Categories
+        data={categories}
+        filterCategories={filterCategories}
+      />
+      <Featured featuredData={featuredData} />
+      <Popular />
+    </ScrollView>
+  )
+}
+
+export default Home
