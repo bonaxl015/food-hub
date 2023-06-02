1f88885fedd5594c8372042b3958f89c5bf7269b
Bon Axl Feeser <bonaxl015@gmail.com>
2023-06-03 00:12:17 +0800

    feat: theme colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const mainColor = '#fe724c'
+export const secondaryColor = '#ffece7'
+export const dark = '#000'
+export const light = '#fff'
+export const gray = '#8c9099'
+export const textGray = '#8a8e9b'
+export const bgGray = '#d6d6d6'
+export const borderGray = '#f6f6f6'
